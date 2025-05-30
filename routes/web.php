643b1669910367326eace110a1e57f1eea609eb5
MeyDoneta29 <bouange45@gmail.com>
2025-05-30 10:45:32 +0100

    creation vue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfesseurController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+Route::get('/professeur', [ProfesseurController::class,'index'])->name('Etudiant.index');
+Route::get('/professeur/create', [ProfesseurController::class,'create'])->name('Etudiant.create');
